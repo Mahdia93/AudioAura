fd34e44e48952f6b4fbed675041ff62092dcfd58
Mahdia Hashimy <mahdia.hashimy@gmail.com>
2017-09-12 22:28:01 -0400

    wtf

--- models/index.js
+++ models/index.js
@@ -8,6 +8,8 @@ var env       = process.env.NODE_ENV || 'development';
 var config    = require(__dirname + '/../config/config.json')[env];
 var db        = {};
 
+var moodsJSON = require('/../config/mood.json');
+
 if (config.use_env_variable) {
   var sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
