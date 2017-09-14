5bc7ea5aa628754bdd6187715ddedee315493ca0
Andrea Pinillos <andreapinillos1@gmail.com>
2017-09-14 11:58:12 -0400

    GRAPPPPHHHHH

--- routes/html-api-routes.js
+++ routes/html-api-routes.js
@@ -7,14 +7,12 @@ module.exports = function(app) {
   // Each of the below routes just handles the HTML page that the user gets sent to.
 
   // index route loads view.html
-  app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../views/index.html"));
+  app.get("/home", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
   });
 
-  app.get("/spotify", function(req, res){
-  	res.render(path.join(__dirname,""))
+  app.get("/chart", function(req, res){
+  	res.sendFile(path.join(__dirname,"../public/chart.html"))
   })
-
-
 
 };
